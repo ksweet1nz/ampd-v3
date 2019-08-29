81cd37646831a299e7fd2ff9cd8547c4f8164978
Kevin Sweet <kevin.sweet.nz@gmail.com>
2019-08-29 16:25:33 +1200

    removed offline plugin for build to work

--- src/components/projects/project-card.js
+++ src/components/projects/project-card.js
@@ -3,24 +3,27 @@ import Image from 'gatsby-image'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
 
+const TitleBlock = styled.div`
+  position: absolute;
+  background-color: var(--main-pop-color);
+  bottom: 0;
+  height: 100%;
+  left: 0;
+  opacity: 0;
+  padding-right: 1rem;
+  right: 0;
+  text-align: right;
+  transition: .5s ease;
+  width: 100%;
+`
 
 const ProjectItem = styled.article`
   height: 100%;
   position: relative;
   width: 100%;
-  
+  &:hover ${TitleBlock} {
+    opacity: 1;
   }
-`
-
-const TitleBlock = styled.div`
-  position: absolute;
-  background: var(--main-pop-color);
-  bottom: 0;
-  padding-right: 1rem;
-  right: 0;
-  text-align: right;
-  width: 100%;
-  z-index: 10;
 `
 
 const StyledLink = styled(Link)`
--- src/pages/index.js
+++ src/pages/index.js
@@ -39,4 +39,3 @@ const Index = () => {
 }
 
 export default Index
-
